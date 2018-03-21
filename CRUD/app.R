7554586928eb6ce4588ffc481848f44575a490b3
frumentum <simon-poeschl@posteo.de>
2018-03-21 16:36:03 +0100

    Add files via upload
    answer from stackoverflow included; extended with another example

--- CRUD/app.R
+++ CRUD/app.R
@@ -4,63 +4,82 @@ library(DT)
 startData <- data.frame(
   town = c("Berlin", "London", "Paris"),
   population = c(3711930, 9787426, 12142802),
+  popCountry = c(82500000, 54000000, 67000000),
   stringsAsFactors = FALSE
 )
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   uiOutput("editDB"),
-   dataTableOutput("table", width = "70%")
+  uiOutput("editDB"),
+  dataTableOutput("table", width = "70%")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   # make data.frame reactive
   rV <- reactiveValues(towns = startData)
-
+  
   # render table output; only single selection is allowed
   output$table <- DT::renderDataTable({
     DT::datatable(rV$towns, selection = "single")
   })
-
+  
   # Reactive function to determine if or which row is selected
   selection <- reactive({
-    if (!is.null(input$table_rows_selected))
-      return(input$table_rows_selected) else return(0)
+    if (!is.null(input$table_rows_selected)) {
+      return(rV$towns[input$table_rows_selected, ]) 
+    } else return(0)
   })
-
+  
   # if a row is selected, one will get the possibility to edit the town;
   # if no row is selected (or it's deselected), edit mode shall disappear
   observeEvent(selection(), {
     output$editDB <- renderUI({
-      if (selection() != 0) {
+      if (is.data.frame(selection())) {
         # which town is selected? create character string
-        selectedTown <- rV$towns$town[input$table_rows_selected]
-
-        # depending on the selectizeInput, textInput occurs
+        selectedTown <- selection()$town
+        # which population is selected?
+        selectedPop <- selection()$population
+        
+        # create UI for editing datatable
         tagList(
-          selectizeInput("town", "Town",
-                         choices = c(unique(rV$towns$town), "new town"),
-                         select = selectedTown),
-          conditionalPanel(
-            condition = "input.town == 'new town'",
-            textInput("text", "New Town")
+          column(4,
+            selectizeInput("town", "Town",
+                           choices = c(unique(rV$towns$town), "new town"),
+                           select = selectedTown,
+                           options = list(create = TRUE)),
+            # actionButton shall appear if selectizeInput changes, no matter if
+            # the town is already known or not
+            conditionalPanel(
+              condition = paste0("input.town != '", selectedTown, "' | ",
+                                 "input.population != '", selectedPop, "'"),
+              actionButton("entry", "save changes")
+            ),
+            br(),
+            hr()
           ),
-          # actionButton shall appear if selectizeInput changes, no matter if
-          # the town is already known or not
-          conditionalPanel(
-            condition = paste0("input.town != '", selectedTown, "'"),
-            actionButton("entry", "save town")
-          ),
-          br(),
-          hr()
+          column(4,
+            sliderInput(
+              "population", "Population", 
+              min = 0,
+              max = selection()$popCountry,
+              value = selectedPop
+            )
+          )
         )
       }
     })
   })
+  
   observeEvent(input$entry, {
-    rV$towns$town[selection()] <- ifelse(input$town == "new town",
-                                         input$text, input$town)
+    newData <- data.frame(
+      town = input$town,
+      population = input$population,
+      stringsAsFactors = FALSE
+    )
+    print(str(newData))
+    rV$towns[input$table_rows_selected, c("town", "population")] <- newData
+      
   })
 }
 
